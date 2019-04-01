f2595552b0fd04a8b86ec71893e27ed1e52e323a
Yashkindjus <43111290+Yashkindjus@users.noreply.github.com>
2019-04-02 00:11:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISDWeek1
+{
+    class MyFactorial
+    {
+        public int MyFact(int f)
+        {
+            int res = 1;
+
+            for (int i = 1; i < f + 1; i++)
+                res = res * i;
+
+            return res;
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int fact, res;
+            MyFactorial Ex = new MyFactorial();
+
+            Console.Title = "Factorial";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Write a number:");
+            fact = Convert.ToInt32(Console.ReadLine());
+            res = Ex.MyFact(fact);
+            Console.WriteLine($"Factorial = {res}");
+
+        }
+    }
+}
